5d6148d67cf6ba01f1f5802dedeb2d57b12657cd
tlholme1 <t.l.holme.1@gmail.com>
2025-08-24 23:17:41 +0100

    Update BuildTeamValuePlot.R

--- R/BuildTeamValuePlot.R
+++ R/BuildTeamValuePlot.R
@@ -6,10 +6,11 @@
 #'
 #' @return A ggplot object representing the team value trend.
 BuildTeamValuePlot <- function(data) {
-  library(ggplot2)
+
   p <- ggplot(data, aes(x = GW, y = team_value)) +
     geom_line(color = 'blue') +
     labs(title = 'Team Value Over Time', x = 'Game Week', y = 'Team Value') +
     theme_minimal()
+  
   return(p)
 }
