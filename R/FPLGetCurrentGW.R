0898fe1afba257e1fa39c95b8b78a43ef305cbcf
tlholme1 <t.l.holme.1@gmail.com>
2025-08-25 00:21:39 +0100

    Create FPLGetCurrentGW.R

---
+++ R/FPLGetCurrentGW.R
@@ -0,0 +1,16 @@
+FPLGetCurrentGW <- function() {
+
+  resp <- httr::GET("https://fantasy.premierleague.com/api/bootstrap-static/")
+  stop_for_status(resp)
+
+  dat <- jsonlite::fromJSON(content(resp, "text", encoding = "UTF-8"), flatten = TRUE)
+  events <- data.table::as.data.table(dat$events)
+
+  Output <- list(
+    current = events[is_current == TRUE, id],
+    `next` = events[is_next == TRUE, id],
+    finished = events[finished == TRUE, max(id, na.rm = TRUE)]
+  )
+
+  return(Output)
+}
