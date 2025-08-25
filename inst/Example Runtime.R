da89fe1d28d41c291ca8f1ec0df2557c1b849668
tlholme1 <t.l.holme.1@gmail.com>
2025-08-25 13:06:24 +0100

    Simplify example runtime script

--- inst/Example Runtime.R
+++ inst/Example Runtime.R
@@ -1,28 +1,16 @@
 library(THFPL)
-library(httr)
-library(ggplot2)
 
-
-Che
 LeagueInfo <- FPLGetLeagueInfo(721349)
 
 CurrentGameWeek <- FPLGetCurrentGW()$current
 
 FPLCompareCaptains(LeagueCode = 721349, GW = CurrentGameWeek)
 
-SelectedPlayers <- FPLGetMostSelectedPlayers(721349,CurrentGameWeek-1)
-
-OldSelectedPlayers <- FPLGetMostSelectedPlayers(721349,CurrentGameWeek)#
+SelectedPlayers <- FPLGetMostSelectedPlayers(721349, CurrentGameWeek - 1)
 
 FPLGetUserTeam(PlayerId = 7572777, GW = 2)
 
-FPLGetSelectedPlayersForALeague(721349,CurrentGameWeek)
-LeagueCode <- 721349
-GW <- 7
-LeagueInfo <- FPLGetLeagueInfo(LeagueCode)
-
-
-FPLGetTransfers(721349,2)
+FPLGetTransfers(721349, 2)
 
 PlotTeamValueOverTime(LeagueCode = 721349,
                       LastGW = 2)
@@ -34,12 +22,9 @@ PlotTeamPointsOverTime(LeagueCode = 721349,
                        LastGW = 2,
                        Average = TRUE)
 
-
-
-SquadsInclBench <- FPLGetSelectedPlayersForALeague(721349,1)
+SquadsInclBench <- FPLGetSelectedPlayersForALeague(721349, 1)
 PlayerPoints <- FPLGetGameweekPoints(1)
 
-SquadsInclBench[PlayerPoints,on = "Name", Points := i.Points]
+SquadsInclBench[PlayerPoints, on = "Name", Points := i.Points]
 
 SquadsInclBench[Status == "Bench", sum(Points), by = player_name]
-
