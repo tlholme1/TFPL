a1de61b56a769828313a95f92f58ef2cf8f805c2
tlholme1 <t.l.holme.1@gmail.com>
2025-08-25 13:44:38 +0100

    Add convenience wrappers for modal XI comparisons

--- R/FPLFindClosestToLeagueModal.R
+++ R/FPLFindClosestToLeagueModal.R
@@ -1,21 +1,12 @@
 #' Compare league teams to the league modal XI
 #'
+#' Convenience wrapper around `FPLFindClosestToModalXI` using the
+#' league modal XI supplier.
+#'
 #' @param LeagueCode League code to query.
 #' @param GW Gameweek number.
 #'
 #' @return A data.table with similarity scores to the league's modal XI.
 FPLFindClosestToLeagueModal <- function(LeagueCode, GW){
-  ModalLeague <- FPLGetLeagueModalXI(LeagueCode, GW)$PlayerId
-  LeagueInfo <- FPLGetLeagueInfo(LeagueCode)
-  setnames(LeagueInfo, "player_name", "PlayerName")
-  PlayerIds <- LeagueInfo$PlayerId
-  names(PlayerIds) <- LeagueInfo$PlayerName
-  Teams <- lapply(PlayerIds, FPLGetUserTeam, GW = GW)
-  Sims <- rbindlist(lapply(names(Teams), function(Nm){
-    Team <- Teams[[Nm]][Status == "Selected", element]
-    data.table(Manager = Nm,
-               Sim = FPLCompareTeams(Team, ModalLeague))
-  }))
-  setorder(Sims, -Sim)
-  return(Sims)
+  FPLFindClosestToModalXI(LeagueCode, GW, FPLGetLeagueModalXI)
 }
